9a99d0fd34d9b215bf01338eb20913f82071852c
kutycma <khacquang2010@gmail.com>
2026-06-12 11:09:27 +0700

    update

--- resources/views/manager.blade.php
+++ resources/views/manager.blade.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi-VN">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Manager Panel' }}</title>
+    <title>{{$title ?? 'Manager Panel'}}</title>
     <script>
-        window.managerSettings = @json([
-            'title' => $title ?? 'Manager Panel',
-            'manager_path' => $manager_path ?? 'manager',
-            'entry' => $entry ?? 'dashboard',
-            'locale' => $locale ?? 'vi'
-        ]);
+        window.managerSettings = {
+            title: @json($title ?? 'Manager Panel'),
+            manager_path: @json($manager_path ?? 'manager'),
+            entry: @json($entry ?? 'dashboard'),
+            locale: @json($locale ?? 'vi-VN'),
+            default_locale: 'vi-VN'
+        };
     </script>
     <script type="module" crossorigin src="/assets/managerzic/assets/index.js"></script>
     <link rel="stylesheet" crossorigin href="/assets/managerzic/assets/index.css">
